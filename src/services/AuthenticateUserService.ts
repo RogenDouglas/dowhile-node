0fe0cddd1adaf45f305e77cf349c262959ae0bc4
rogendouglas <rogendouglas@hotmail.com>
2021-10-23 16:49:15 -0500

    feat: performed github access token search and user insertion in the database.

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,21 +1,82 @@
 import axios from "axios";
+import { response } from "express";
+import { sign } from "jsonwebtoken";
+import prismaCliente from "../prisma";
+
+interface IAccessTokenResponse {
+  access_token: string;
+}
+
+interface IUserResponse {
+  id: number;
+  login: string;
+  name: string;
+  avatar_url: string;
+}
 
 class AuthenticateUserService {
   async execute(code: string) {
     const url = "https://github.com/login/oauth/access_token";
 
-    const response = await axios.post(url, null, {
-      params: {
-        client_id: process.env.GITHUB_CLIENT_ID,
-        client_secret: process.env.GITHUB_CLIENT_SECRET,
-        code,
-      },
-      headers: {
-        Accept: "application/json",
+    const { data: accessToken } = await axios.post<IAccessTokenResponse>(
+      url,
+      null,
+      {
+        params: {
+          client_id: process.env.GITHUB_CLIENT_ID,
+          client_secret: process.env.GITHUB_CLIENT_SECRET,
+          code,
+        },
+        headers: {
+          Accept: "application/json",
+        },
+      }
+    );
+
+    const response = await axios.get<IUserResponse>(
+      "https://api.github.com/user",
+      {
+        headers: {
+          authorization: `Bearer ${accessToken.access_token}`,
+        },
+      }
+    );
+
+    const { id, login, name, avatar_url } = response.data;
+
+    let user = await prismaCliente.user.findFirst({
+      where: {
+        github_id: id,
       },
     });
 
-    return response.data;
+    if (!user) {
+      user = await prismaCliente.user.create({
+        data: {
+          github_id: id,
+          name,
+          login,
+          avatar_url,
+        },
+      });
+    }
+
+    const token = sign(
+      {
+        user: {
+          name: user.name,
+          avatar_url: user.avatar_url,
+          id: user.id,
+        },
+      },
+      process.env.JWT_SECRET,
+      {
+        subject: user.id,
+        expiresIn: "1d",
+      }
+    );
+
+    return { token, user };
   }
 }
 
