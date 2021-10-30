27183a362748248d963311f1b0f88576f087cdfc
rogendouglas <rogendouglas@hotmail.com>
2021-10-30 15:44:11 -0500

    feat: created requests for messages and profile.

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,26 @@
 import { Router } from "express";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
+import { CreateMessageController } from "./controllers/CreateMessageController";
+import { GetLastMessagesController } from "./controllers/GetLastMessagesController";
+import { ProfileUserController } from "./controllers/ProfileUserController";
+import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
 
 const routes = Router();
 
 routes.post("/authenticate", new AuthenticateUserController().handle);
+
+routes.post(
+  "/messages",
+  ensureAuthenticated,
+  new CreateMessageController().handle
+);
+
+routes.get("/messages/last", new GetLastMessagesController().handle);
+
+routes.get(
+  "/user/profile",
+  ensureAuthenticated,
+  new ProfileUserController().handle
+);
 
 export { routes };
